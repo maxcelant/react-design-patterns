b9a769f34c0ceafa9e283223259cf5c19812b147
Max Celant <maxcelant@gmail.com>
2024-03-22 14:12:45 -0500

    hoc

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import MyForm from "./Form/MyForm";
+import MyForm from "./CompoundComponent/MyForm";
+import { StyledButton, StyledText } from "./HigherOrderComponent/withStyles";
 
 function App() {
   return (
     <div className="App">
+      <StyledButton />
+      <StyledText />
       <MyForm />
     </div>
   );
